9b6cfec651308d8b2ae95aa557e7be5c592b3881
SumPol <polya_star95@mail.ru>
2025-04-24 21:31:23 +0300

    Added HW #9

---
+++ pages/modal_dialogs.py
@@ -0,0 +1,13 @@
+from pages.base_page import BasePage
+from pages.elements_page import WebElement
+
+
+class ModalDialogs(BasePage):
+
+
+    def __init__(self, driver):
+        self.base_url = 'https://demoqa.com/modal-dialogs'
+        super().__init__(driver, self.base_url)
+
+        self.btns_third_menu = WebElement(driver, 'div:nth-child(3) > div > ul > li')
+        self.icon = WebElement(driver, '#app > header > a')
